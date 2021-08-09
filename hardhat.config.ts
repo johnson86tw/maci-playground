16219dbf668a1c84dc668fce42bc50552136bfae
Johnson Chen <chnejohnson@gmail.com>
2021-08-09 11:48:46 +0800

    feat: MACIFactory

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,9 @@
-import { task } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
+import fs from "fs";
+import path from "path";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -13,9 +15,45 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+// Copy Poseidon artifacts from maci-contracts
+task("compile", "Compiles the entire project, building all artifacts", async (_, { config }, runSuper) => {
+  await runSuper();
 
-export default {
-  solidity: "0.8.4",
+  const poseidons = ["PoseidonT3", "PoseidonT6"];
+  for (const contractName of poseidons) {
+    const artifact = JSON.parse(
+      fs.readFileSync(`./node_modules/maci-contracts/compiled/${contractName}.json`).toString(),
+    );
+    fs.writeFileSync(
+      path.join(config.paths.artifacts, `${contractName}.json`),
+      JSON.stringify({ ...artifact, linkReferences: {} }),
+    );
+    console.log(`Successfully copied ${contractName}.json`);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: {
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 20,
+      },
+    },
+    compilers: [
+      {
+        version: "0.8.0", // only for Greeter.sol
+      },
+      {
+        version: "0.6.12",
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true, // essential of maciFactory.ts
+    },
+  },
 };
+
+export default config;
